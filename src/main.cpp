84d159ef4a892ddd292aec07d397b64d80a83141
Tim Stullich <tstullich@protonmail.com>
2020-04-01 14:50:19 +0200

    Cleaned up build scripts and added trace() function

--- src/main.cpp
+++ src/main.cpp
@@ -7,6 +7,7 @@
 int main(int argc, char** argv) {
   try {
     gm::Integrator integrator;
+    integrator.trace();
   } catch (std::exception& e) {
     std::cerr << e.what() << std::endl;
     return EXIT_FAILURE;
